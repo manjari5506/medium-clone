2a252071aad2d5b02d0442c1585b6be2d3369bf7
Manjari <raikwar.manjari@gmail.com>
2023-09-04 17:29:00 +0530

    minor changes

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require("./User");
-//const storiesRouter = require("./Stories");
+const storiesRouter = require("./Stories");
 
 router.get("/", (req, res) => {
   res.send("This api is reserved for medium clone");
